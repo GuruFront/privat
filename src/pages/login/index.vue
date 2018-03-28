448f8589d6ed08c320db5f63ddfc5e63c66478e6
gurufront <ruslan.gunder@gmail.com>
2018-03-28 19:41:00 +0300

    Start empty template

---
+++ src/pages/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>Main</h1>
+</template>
+<script>
+export default {
+  name: 'HelloWorld'
+}
+
+</script>
+<style lang="scss">
+@import '../main.scss';
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/pages/login'
+
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
